7710d2821113cac9a40449f369fac4d4963dc502
mohammad11-hub <mrmdsunasara@gmail.com>
2026-05-07 12:53:08 +0530

    variablrna values

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const thirdbutton = document.querySelector(".thirdbutton");
-thirdbutton.addEventListener("click", () => {
-  alert("thanks for the click third button❤️");
-});
+// const thirdbutton = document.querySelector(".thirdbutton");
+// thirdbutton.addEventListener("click", () => {
+//   alert("thanks for the click third button❤️");
+// });
+
+console.log("hello world");
+// it is the variable and values
+
+var Myname = "satyarth";
+console.log(Myname);
