fe8ef2ae739fef1aac7f69e3aadd3ebeb260b9aa
mohammad11-hub <mrmdsunasara@gmail.com>
2026-05-10 09:48:27 +0530

    complete if_else condition

--- script.js
+++ script.js
@@ -8,8 +8,47 @@ console.log("hello world");
 
 var My_name = "10";
 
+// challange no 1
+
 // var _name$ = "satyarth";
 // var 123name = "satyarth";
 // var $name = "satyarth";
 // var my@email = "satyarth";
 console.log( typeof +My_name);
+
+// expession and statement
+// assigmrnt operator
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log(c);
+
+// challange no 2
+
+// var numebr ="hello " /2;
+// console.log(numebr);
+
+// var result = 0.1 + 0.2;
+// console.log(result);
+
+// trip question
+
+// var temp = 1;
+// if(temp >= 30){
+//     console.log("dariya mu nava joi hu");
+// }else if(temp>= 15 && temp < 30){
+//     console.log("hotel per TV jovoy");
+// }
+// else {
+//     console.log("nake gerjato roy");
+// }
+
+// votting task 
+
+var age = 18;
+
+if (age >= 18) {
+  console.log("you are eligible to vote");
+}else if(age <= 18){
+    console.log("you are not eligible to vote");
+}
